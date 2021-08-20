dd9081b02b46616edf5921e47ca2a1162b032a39
iviko1 <iveri.kobakhidze.1@btu.edu.ge>
2021-08-21 02:24:39 +0400

    final

---
+++ js/script.js
@@ -0,0 +1,23 @@
+const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
+const header = document.querySelector(".header");
+const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
+
+btnHamburger.addEventListener("click", () => {
+  if (header.classList.contains("open")) {
+    body.classList.remove("noscroll");
+    header.classList.remove("open");
+    fadeElems.forEach((elem) => {
+      elem.classList.remove("fade-in");
+      elem.classList.add("fade-out");
+    });
+  } else {
+    body.classList.add("noscroll");
+    header.classList.add("open");
+    fadeElems.forEach((elem) => {
+      elem.classList.add("fade-in");
+      elem.classList.remove("fade-out");
+    });
+  }
+});
